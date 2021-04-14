02e1ca42bda28b69fc38a2742854550bcc01d217
Virginija <>
2021-04-14 16:41:46 +0000

    add message here

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # COM411-Problem-Solving-through-programing-new-
+
+Week 1 - inputs/outputs and setups
---
+++ main.py
@@ -0,0 +1,5 @@
+print('EA Sports server are trash')
+print("First step: git add .")
+print("Second step: git commit -m \"Your message here\"")
+print("Last step: git push")
+
