6e03ef32459ea88eadabb2540b57398cf9f40959
Virginija <>
2021-04-14 23:53:13 +0000

    help Beep to create BMI

---
+++ basics/input/data_types.py
@@ -0,0 +1,11 @@
+#create program which allows to calculate your BMI (body-mass index)
+
+print("What is your name human?")
+name = input()
+print("How old are you? (in years)")
+age = int(input())
+print("How tall are you? (in  meters)")
+height = float(input())
+print("How much do you weigh? (in kilograms)")
+weight = float(input())
+#how to calculate BMI???
