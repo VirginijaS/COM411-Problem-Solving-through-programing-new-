e6ecfc08ffd6216885a45c176cac3e275186c0c5
Virginija <>
2021-04-14 16:48:09 +0000

    displayed message to standart output in miltiplelines

---
+++ basics/output/multiline_message.py
@@ -0,0 +1,7 @@
+#Display message to standart output in multiple lines of code
+print("System Failure Imminent!")
+print("System Reboot has been initiated...\n")
+print("...rebooting sensory system")
+print("...rebootong output motors")
+print("...rebooting hover engine\n")
+print("System is online")
