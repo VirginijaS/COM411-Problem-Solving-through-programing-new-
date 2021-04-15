f3030e2f8ac93b221c1c8c453cc245a5b92c1260
Virginija <>
2021-04-15 18:58:16 +0000

    created basic input/output program

---
+++ basics/input/review.py
@@ -0,0 +1,8 @@
+#Create review about input/output
+print("What is your name?")
+name = input()
+print("What is your age?")
+age = input()
+print("How many time to print symbol?")
+symbol = int(input())
+print(f"Symbol printed {'♥' * symbol}")
--- basics/input/user_age.py
+++
@@ -1,4 +0,0 @@
-#print user age
-print("What is your age?")
-age = input()
-print("Your age is" , age)
