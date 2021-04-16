81682b8283502b751814f06cd5498d3333aec9d0
Virginija <>
2021-04-16 19:26:20 +0000

    completed with if, elif and else

---
+++ Demos/ifelse.py
@@ -0,0 +1,8 @@
+a = int(input())
+print("Can you write second number?")
+b = int(input())
+if a  > b:
+  print("First is bigger")
+else:
+  print("First is equal or smaller!")
+print("Done")
--- main.py
+++ main.py
@@ -5,7 +5,10 @@ a = int(input())
 print("Can you write second number?")
 b = int(input())
 if a  > b:
-  print("First is bigger")
+  print("First is bigger\n")
+elif a < b:
+  print("First is smaller\n")
 else:
-  print("First is equal or smaller!")
+  print("Both are equal\n")
+
 print("Done")
