ef6f79a590da1f2386a3f2008eb9324ada117a0e
Virginija <>
2021-04-19 18:19:22 +0000

    we done integres and nad find prime

---
+++ Demos/W3/countdown.py
@@ -0,0 +1,16 @@
+#While loop to print a countdown of numbers, froma user defined value e.g. if user gives me 5, my code will print 5,4,3,2,1 BOOM
+
+print("What is the starting point?")
+start = int(input()) #control variable/counter
+start_copy = start
+
+
+while(start>=1):   #condition that needs to be satisfied for loop continue
+  print(start)
+  start-=1 #modification to counter variable to avoid infinite loop
+print("BOOM!!!")
+
+
+for counter in range(start_copy,0, -1):
+  print(counter)
+print("BOOM!!!!")
