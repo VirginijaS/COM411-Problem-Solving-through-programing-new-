251961d1e2687b18acdec1dbb2f1ad385300f9e8
Virginija <>
2021-04-19 12:46:59 +0000

    decreasing numbers

--- main.py
+++ main.py
@@ -1,28 +1,9 @@
-#Code that can be used to calculate the total cost od room akeover in a house
+#While loop to print a countdown of numbers, froma user defined value e.g. if user gives me 5, my code will print 5,4,3,2,1 BOOM
 
-def room_area(width, lenght):
-  return width*lenght
+print("What is the starting point?")
+start = int(input())
 
-def room_name():
-  print("What is the of the room?")
-  return input()
-
-def price(name,area):
-  if name == "bathroom":
-    return 20*area
-  elif name == "kitchen":
-    return 10*area
-  elif name == "bedroom":
-    return 5*area 
-  else:
-    return 7*area
-
-def run():
-  name = room_name()
-  print("What is the width of the room?")
-  w = float(input())
-  print("What is the lenght of the room?")
-  l = float(input())
-  print(f"You should pay £{price(name,room_area(w,l)):.2f}")
-
-run()
+while(start>=1):
+  print(start)
+  start-= #start =
+print("BOOM!!!")
