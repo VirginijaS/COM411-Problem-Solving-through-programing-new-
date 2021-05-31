75ef7501e46b427736acec5583c4a9ddf8ceffbf
Virginija <>
2021-05-31 00:23:44 +0000

    book cover

---
+++ basics/decissions/nested_decision/nested.py
@@ -0,0 +1,16 @@
+# ask user what to do
+print("What type of cover does the book have?")
+cover = input()
+
+if (cover == "soft"):
+  print("Is the book perfect-bound?")
+  perfect_bound = input()
+
+  if(perfect_bound == "yes"):
+    print("Soft cover, perfect bound books are very popular!")
+  else:
+        print("Soft covers with coils or stitches are great for short books") 
+else:
+    print("Books with hard covers can be more expensive!")
+ 
+
